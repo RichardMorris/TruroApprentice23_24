a50a93da720105ea0d22e4305ea3ae18fd66d4e2
Richard Morris <rich@singsurf.org>
2023-11-14 12:36:38 +0000

    add a benchmark test

--- testingcode.cpp
+++ testingcode.cpp
@@ -1,6 +1,8 @@
 #include "weighted_graph.h"
 using namespace std;
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_all.hpp>
+#include <algorithm>    // std::fill
+#include <vector>       // std::vector
 
 TEST_CASE("Build graph")
 {
@@ -35,3 +37,31 @@ TEST_CASE("Build graph")
   dist = g.dijkstra(&A, &A);
   REQUIRE(0 == dist);
 }
+
+TEST_CASE("Collection times") {
+    const int size = 10000; // Size of the vector
+    const int lower_bound = 1; // Lower bound of the random numbers
+    const int upper_bound = 100000; // Upper bound of the random numbers
+
+    // Seed for the random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Generate a vector of unique random numbers
+    std::vector<int> randomNumbers;
+    randomNumbers.reserve(size);
+
+    std::uniform_int_distribution<int> distribution(lower_bound, upper_bound);
+
+    BENCHMARK("Build random vector") {
+    while (randomNumbers.size() < size) {
+        int randomNum = distribution(gen);
+        if (std::find(randomNumbers.begin(), randomNumbers.end(), randomNum) == randomNumbers.end()) {
+            randomNumbers.push_back(randomNum);
+        }
+    }
+    };
+
+
+}
+
